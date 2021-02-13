04865fd1df01b9ff04ec8f805af0c82b07aa9e9c
2293736867 <2293736867@qq.com>
2021-02-13 11:48:48 +0800

    V1.0

---
+++ js/icon.js
@@ -0,0 +1,51 @@
+const codeLogoSize = "20"
+
+const base64Logo = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+    "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" +
+    "<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" width=\""+codeLogoSize+"px\" height=\""+codeLogoSize+"px\" viewBox=\"0 0 "+codeLogoSize+" "+codeLogoSize+"\" enable-background=\"new 0 0 "+codeLogoSize+" "+codeLogoSize+"\" xml:space=\"preserve\">  <image id=\"image0\" width=\""+codeLogoSize+"\" height=\""+codeLogoSize+"\" x=\"0\" y=\"0\"\n" +
+    "    href=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAABeGlDQ1BpY2MAAHjalZE5SANBGIU/\n" +
+    "V0UJEQtTiFpsoVYKoiKWGoUgRAhRwatwd2OikF3DboKNpWAbsPBovAoba20tbAVB8ACxs7NStBFZ\n" +
+    "/0kCCUIEB4b5eDPvMfMGtMO0ZXt1fWA7WTceCeuzc/N6wwsaAYJAu2F5mdFYLErV8XlHjVpve1UW\n" +
+    "/xtNiWXPghpdeMTKuFnhJeGh9WxG8a5wyFoxEsJnwj2uXFD4QelmkV8VpwqsqcyQOx0fEw4J66kK\n" +
+    "NivYWnFt4UHhzoTtSL42W+SE4g3Fdjpnle6pXhhcdmamlC6zgwgTTBJDxyTHKmmy9MrqiOIRl/1w\n" +
+    "FX9bwR8TlymuVSxxjLOGjVHwo/7gd7decqC/mBQMQ/2z7793QcM2fOd9/+vI97+PofYJLp2yf+0Q\n" +
+    "hj9Ez5e1zgNo3oTzq7Jm7sDFFrQ+ZgzXKEi1MrVkEt5O5ZvmoOUGAgvF3kr7nNzDtHQVvYa9fehO\n" +
+    "SfZilXc3Vvb255lSfz/r+nJxppvKewAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6\n" +
+    "mAAAF3CculE8AAABwlBMVEX///8+ir1FlMpVtPRTsvE7ib1RsfJRsvI4hrxPsfM0hLswk9YwktUw\n" +
+    "grkxg7ouktUuktUuktUtkdQtgLgugbkvfLArkNUrkNUrkNQqfrYrf7grf7csfbQpj9QnjtQoea8o\n" +
+    "fbYofrcljdMle7Ule7UjjNMijNMidq4jerUeda8ed7MeitIdidIbdbIbiNEah9AXc7EYcq4Yh9EY\n" +
+    "h9EXhtEVcrAVcrAVcrAWhdAVhdAUhM8UgcoTcK8ScK8ScK8TcK8ThNASg9ARfMQQb64Qb64Qgs8P\n" +
+    "gs8MgM4Jf84mn/AFfc0jne8jnvADe8oOgM8hnfAhm+07ibw7hLRTs/M4h7w4hrs4grNQsvM1hbs1\n" +
+    "hLo1gLJOsfMyg7oyg7kyfrBLr/MugbkugbhIrvMrkNUrf7hFrfMoj9QofLQofbZCrPIljdMlis4l\n" +
+    "ebMleLEle7VAqvIijNMii9EifLk9qfIeidAeitI5qPIbdbIbc64bhs0biNE3pvIYc7EYca0Yh9E0\n" +
+    "pfEVcrAVhdAxpPESg9ASg88vo/EOgc8Ogc4OesIrofELgM4Lf80LeMEpoPEIfs4Ifs0IdsAmn/AF\n" +
+    "fMwFdMAjnvD////urbODAAAAUXRSTlMAY/CjLGa4PY2smRQKCrM85+A1CsLN8/VwHs2WCnCs4PVY\n" +
+    "ZtYnVfWkClX1pApm1idwrOD1WfP1cB7Nlgo85+A0CsLNFAoKs5mNrGa4PWPwoywHaBscAAAAAWJL\n" +
+    "R0QAiAUdSAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAAd0SU1FB+UCCwUbGJsNZdcAAAEMSURBVBjT\n" +
+    "XdBlUwJRGAXgY7fYhYHdYrdgd2OgropirL0qirEi1qogJj/Y9+XTwvPlzJyZe+/cA6gFBYeEIkDY\n" +
+    "zKzJFB7h10XOzS8skihVF202Ly2vMCAm1lfFxQuCsLq2zqBJSEyiLjnFQjZSNxnSrNb0DGRmbTFt\n" +
+    "9jZDzg7JzdsVRVGXD+wxoGCfHBweHRcW0TUnjLK4RJKk07PSMn7vnFGWV9guSGUVl5cMqLZf2a9v\n" +
+    "SI2eyluGWlmW7+rqHaShEbhnaHI6m1vQ2vbA2jseGTq7DEY61t3zRJ57XxjQ1+/75sCgoiivb+9M\n" +
+    "NciQy+X+8DD1dMOfX98/ZMRv0NHfP693bDxg+onJqWmKf7GKW7KeP3YgAAAAJXRFWHRkYXRlOmNy\n" +
+    "ZWF0ZQAyMDIxLTAyLTEwVDIxOjI3OjI0KzA4OjAwHTB7kQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAy\n" +
+    "MS0wMi0xMFQyMToyNzoyNCswODowMGxtwy0AAAAadEVYdGV4aWY6Qml0c1BlclNhbXBsZQA4LCA4\n" +
+    "LCA4Eu0+JwAAABF0RVh0ZXhpZjpDb2xvclNwYWNlADEPmwJJAAAAIXRFWHRleGlmOkRhdGVUaW1l\n" +
+    "ADIwMjE6MDI6MTAgMjE6Mjc6MDCR9D4SAAAAE3RFWHRleGlmOkV4aWZPZmZzZXQAMTkwTI7zwgAA\n" +
+    "ABN0RVh0ZXhpZjpJbWFnZUxlbmd0aAAyMLrwl1oAAAASdEVYdGV4aWY6SW1hZ2VXaWR0aAAyMBAU\n" +
+    "4T0AAAAadEVYdGV4aWY6U29mdHdhcmUAR0lNUCAyLjEwLjIy6B7n7gAAACR0RVh0ZXhpZjp0aHVt\n" +
+    "Ym5haWw6Qml0c1BlclNhbXBsZQA4LCA4LCA4IBv0UwAAABx0RVh0ZXhpZjp0aHVtYm5haWw6Q29t\n" +
+    "cHJlc3Npb24ANvllcFcAAAAedEVYdGV4aWY6dGh1bWJuYWlsOkltYWdlTGVuZ3RoADI1NlBwMAMA\n" +
+    "AAAddEVYdGV4aWY6dGh1bWJuYWlsOkltYWdlV2lkdGgAMjU2iAb6FAAAACh0RVh0ZXhpZjp0aHVt\n" +
+    "Ym5haWw6SlBFR0ludGVyY2hhbmdlRm9ybWF0ADMxNltSnwUAAAAvdEVYdGV4aWY6dGh1bWJuYWls\n" +
+    "OkpQRUdJbnRlcmNoYW5nZUZvcm1hdExlbmd0aAA1MjM2Bs/wVAAAACp0RVh0ZXhpZjp0aHVtYm5h\n" +
+    "aWw6UGhvdG9tZXRyaWNJbnRlcnByZXRhdGlvbgA2EhWKGgAAACB0RVh0ZXhpZjp0aHVtYm5haWw6\n" +
+    "U2FtcGxlc1BlclBpeGVsADPh181aAAAAG3RFWHRpY2M6Y29weXJpZ2h0AFB1YmxpYyBEb21haW62\n" +
+    "kTFbAAAAInRFWHRpY2M6ZGVzY3JpcHRpb24AR0lNUCBidWlsdC1pbiBzUkdCTGdBEwAAABV0RVh0\n" +
+    "aWNjOm1hbnVmYWN0dXJlcgBHSU1QTJ6QygAAAA50RVh0aWNjOm1vZGVsAHNSR0JbYElDAAAAIHRF\n" +
+    "WHRzb2Z0d2FyZQBodHRwczovL2ltYWdlbWFnaWNrLm9yZ7zPHZ0AAAAYdEVYdFRodW1iOjpEb2N1\n" +
+    "bWVudDo6UGFnZXMAMaf/uy8AAAAXdEVYdFRodW1iOjpJbWFnZTo6SGVpZ2h0ADIw3+GomQAAABZ0\n" +
+    "RVh0VGh1bWI6OkltYWdlOjpXaWR0aAAyMCdOaBQAAAAZdEVYdFRodW1iOjpNaW1ldHlwZQBpbWFn\n" +
+    "ZS9wbmc/slZOAAAAF3RFWHRUaHVtYjo6TVRpbWUAMTYxMjk2MzY0NIRMGP0AAAATdEVYdFRodW1i\n" +
+    "OjpTaXplADEwNjU0QkKqjsmlAAAARXRFWHRUaHVtYjo6VVJJAGZpbGU6Ly8vYXBwL3RtcC9pbWFn\n" +
+    "ZWxjL2ltZ3ZpZXcyXzlfMTYwOTkwMzUxMzY4MzQ5MzdfMV9bMF11KWURAAAAAElFTkSuQmCC\" ></image>\n" +
+    "</svg>\n"
---
+++ js/init.js
@@ -0,0 +1,19 @@
+let list = document.getElementsByClassName("pagehead-actions")
+
+if (list.length > 0) {
+    list = list[0]
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+
+    a.href = 'https://github1s.com/' + window.location.href.split('github.com')[1]
+    a.target = '_black'
+    a.className = 'btn btn-sm tooltipped tooltipped-s'
+    a.style.height = '28px'
+    a.style.paddingBottom = '0'
+    a.style.paddingTop = '2px'
+    a.innerHTML = base64Logo
+    a.setAttribute('aria-label','Open with VSCode')
+
+    li.append(a)
+    list.insertBefore(li, list.getElementsByTagName("li")[0])
+}
