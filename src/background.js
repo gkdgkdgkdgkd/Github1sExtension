91010c89d83fd5c63148468d918b9d515a08782b
2293736867 <2293736867@qq.com>
2021-02-20 19:51:14 +0800

    V1.1

--- src/background.js
+++ src/background.js
@@ -1,7 +1,22 @@
 chrome.runtime.onInstalled.addListener(()=>{
+    const url = chrome.runtime.getURL('')
+    const tab = chrome.tabs.create({url})
+    console.log(tab)
+    console.log('???')
     chrome.contextMenus.create({
         id:'32423',
         title:'324',
-        contexts:['page']
+        type:'normal',
+        contexts:['page'],
+    })
+
+    chrome.contextMenus.onClicked.addListener(()=>{
+        console.log('click')
     })
 })
+
+function aa(info,tab){
+    console.log(info)
+    console.log(tab)
+    console.log('ccc')
+}
